9f6e89475567e386b8b5d88b44b3caad994e0967
BITCAMP <gimjuyeong512@gmail.com>
2024-07-10 18:42:03 +0900

    change03

--- src/main/java/com/nc13/spring_legacy/controller/HomeController.java
+++ src/main/java/com/nc13/spring_legacy/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.nc13.spring_legacy.controller;
 
 import com.nc13.spring_legacy.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
